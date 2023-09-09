c82ff497881e8340e55c4b2246430c8a13e560a4
netorapg <renatoplatz11@outlook.com>
2023-09-09 14:42:17 -0300

    new update

--- prototipo3.c
+++ prototipo3.c
@@ -1,47 +1,45 @@
 #include <stdio.h>
-#include <stdint.h>
 
-#define REG_SIZE 5
 #define MEM_SIZE 256
+#define REG_SIZE 8
 
-uint16_t vetor[REG_SIZE];
-uint16_t memory[MEM_SIZE];
+typedef enum {
+    ADD,
+    SUB,
+    LOAD,
+    STORE
+} InstructionType;
 
-// Definição das instruções
-#define INC 0x1000
-#define ADD 0x2000
-#define SUB 0x3000
-#define LOAD 0x4000
-#define STORE 0x5000
-#define HALT 0xFFFF
+typedef struct {
+    InstructionType type;
+    int op1;
+    int op2;
+    int dest;
+} Instruction;
 
+Instruction memory[MEM_SIZE];
+int registers[REG_SIZE];
+Instruction instr;
+
+// Função para buscar a instrução na memória
 void fetch(int PC) {
-    vetor[0] = memory[PC];
+    instr = memory[PC];
 }
 
+// Função para decodificar e executar a instrução
 void execute() {
-    uint16_t opcode = vetor[0] & 0xF000;
-    uint16_t op1 = (vetor[0] >> 8) & 0xF;
-    uint16_t op2 = (vetor[0] >> 4) & 0xF;
-    uint16_t dest = vetor[0] & 0xF;
-
-    switch (opcode) {
-        case INC:
-            vetor[dest]++;
-            break;
+    switch (instr.type) {
         case ADD:
-            vetor[dest] = vetor[op1] + vetor[op2];
+            registers[instr.dest] = registers[instr.op1] + registers[instr.op2];
             break;
         case SUB:
-            vetor[dest] = vetor[op1] - vetor[op2];
+            registers[instr.dest] = registers[instr.op1] - registers[instr.op2];
             break;
         case LOAD:
-            vetor[dest] = memory[op1];
+            registers[instr.dest] = memory[instr.op1].type;
             break;
         case STORE:
-            memory[op1] = vetor[dest];
-            break;
-        case HALT:
+            memory[instr.op1].type = registers[instr.dest];
             break;
         default:
             printf("Instrução inválida\n");
@@ -50,28 +48,69 @@ void execute() {
 }
 
 int main() {
-    for (int i = 0; i < REG_SIZE; i++) {
-        vetor[i] = 0;
-    }
+    // Inicializando a memória e os registradores
     for (int i = 0; i < MEM_SIZE; i++) {
-        memory[i] = HALT;
+        memory[i].type = LOAD;  // Inicializa com a instrução LOAD em vez de HALT
+    }
+    for (int i = 0; i < REG_SIZE; i++) {
+        registers[i] = 0;
     }
 
-    // Carregue suas instruções na memória aqui
-    memory[0] = INC | (0 << 8) | (0 << 4) | 1;  // Incrementa o registro 0 e armazena em 1
+    printf("RO: %d, R1: %d, R2: %d, R3: %d, R4: %d, R5: %d, R6: %d, R7: %d, R8: %d\n", registers[0], registers[1], registers[2], registers[3], registers[4], registers[5], registers[6], registers[7]);
 
+/*  // Instrução 1: LOAD valor da memória[10 em R1
+    instr.type = LOAD;
+    instr.op1 = 10;
+    instr.dest = 1;
+    memory[0] = instr;
+
+    // Instrução 2: LOAD valor da memória[11] em R2
+    instr.type = LOAD;
+    instr.op1 = 11;
+    instr.dest = 2;
+    memory[1] = instr;
+
+    // Exemplo: instrução para adicionar conteúdo de R1 e R2 e armazenar em R3
+    instr.type = ADD;
+    instr.op1 = 1;
+    instr.op2 = 2;
+    instr.dest = 3;
+    memory[2] = instr;
+*/
+  
+    memory[20].type = 6;
+    memory[21].type = 7;
+
+    instr.type = LOAD;
+    instr.op1 = 20;
+    instr.dest = 4;
+    memory[3] = instr;
+
+    instr.type = LOAD;
+    instr.op1 = 21;
+    instr.dest = 5;
+    memory[4] = instr;
+
+    instr.type = ADD;
+    instr.op1 = 4;
+    instr.op2 = 5;
+    instr.dest = 6;
+    memory[5] = instr;
+
+    // Loop de execução até o final da memória
     int PC = 0;
-    do {
+    while (PC < MEM_SIZE) {
         fetch(PC);
         execute();
-
+        
+      // printf("RO: %d, R1: %d, R2: %d, R3: %d, R4: %d, R5: %d, R6: %d, R7: %d, R8: %d\n", registers[0], registers[1], registers[2], registers[3], registers[4], registers[5], registers[6], registers[7]);
+        
         PC++;
-    } while (vetor[0] != HALT);
-
-    printf("Estado final dos registros (vetor):\n");
-    for (int i = 0; i < REG_SIZE; i++) {
-        printf("R%d: %u\n", i, vetor[i]);
     }
 
+    printf("RO: %d, R1: %d, R2: %d, R3: %d, R4: %d, R5: %d, R6: %d, R7: %d, R8: %d\n", registers[0], registers[1], registers[2], registers[3], registers[4], registers[5], registers[6], registers[7]);
+
+
+    printf("Resultado da soma: R6 = %d\n", registers[6]);
     return 0;
 }
