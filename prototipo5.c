dc5541109de53da2579badd4c632688b5d03b070
netorapg <renatoplatz11@outlook.com>
2023-10-02 15:32:47 -0300

    new update

--- prototipo5.c
+++ prototipo5.c
@@ -198,7 +198,10 @@ int main ()
 		uint16_t formato = extract_bits(instrucao, 15, 1); // Extrai o bit de formato da instrução	
 		if(formato == 0){ // Se o formato for 0, a instrução é do tipo R
 			executarInstrucaoR(instrucao);
-			pc++;
+
+			if(extract_bits(instrucao, 9, 6) != 63){
+				pc++;
+			}
 		}
 		else { // Se o formato for 1, a instrução é do tipo I
 			executarInstrucaoI(instrucao);
